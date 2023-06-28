02f352f2242b3ca19de2a20875c4342d91b32d34
Ozod <bukhorizodao@gmail.com>
2023-06-28 20:58:11 +0500

    added bcg-image and fixed header

--- src/initial-load-page.js
+++ src/initial-load-page.js
@@ -2,10 +2,20 @@ function createHeader() {
     const header = document.createElement('header');
     header.setAttribute('id', 'header');
 
+    const headerContainer = document.createElement('div');
+    headerContainer.classList.add('header-container');
+
     const h1 = document.createElement('h1');
+    h1.classList.add('h1-header');
     h1.textContent = 'To-Do-App';
 
-    header.appendChild(h1);
+    const checkedImage = document.createElement('img');
+    checkedImage.classList.add('checked-img');
+    checkedImage.src = './images/checked-image.png';
+
+    headerContainer.appendChild(h1);
+    headerContainer.appendChild(checkedImage);
+    header.appendChild(headerContainer);
     return header;
 }
 
