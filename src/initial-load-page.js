2dd9e6426adcd82d838014cd461de18b07e5c714
Ozod <bukhorizodao@gmail.com>
2023-06-28 20:36:25 +0500

    removed side-bar

--- src/initial-load-page.js
+++ src/initial-load-page.js
@@ -8,35 +8,10 @@ function createHeader() {
     header.appendChild(h1);
     return header;
 }
-function createNav() {
-    const nav = document.createElement('nav');
-    nav.classList.add('side-bar');
-    nav.innerHTML = `
-    <ul class="nav">
-    <li>
-    <span class="span">Home</span>
-    </li>
-    <li>
-    <span class="span">Today</span>
-    </li>
-    <li>
-    <span class="span">Projects</span>
-    <ul class="projects">
-    <li>Coding</li>
-    <li>Work out</li>
-    </ul>
-    </li>
-    </ul>
-    `;
-    return nav;
-}
 
 function loadPage() {
     const content = document.querySelector('#content');
     const header = createHeader();
     content.appendChild(header);
-
-    const nav = createNav();
-    content.appendChild(nav);
 }
 export default loadPage;
