a9a94aeb04855935697c4f50948574f33d04f4d9
Ozod <bukhorizodao@gmail.com>
2023-06-28 23:49:43 +0500

    created side bar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import loadPage from "./initial-load-page";
 
+
+function displayHomeOrNotes() {
+    const listHome = document.querySelector('.list-home');
+    const listNotes = document.querySelector('.list-notes');
+
+    listHome.addEventListener('click', () => {
+        listHome.classList.add('active');
+        listNotes.classList.remove('active');
+    })
+    listNotes.addEventListener('click', () => {
+        listNotes.classList.add('active');
+        listHome.classList.remove('active');
+    });
+}
 function showPage() {
     loadPage();
+    displayHomeOrNotes();
 }
 showPage();
--- src/initial-load-page.js
+++ src/initial-load-page.js
@@ -18,10 +18,35 @@ function createHeader() {
     header.appendChild(headerContainer);
     return header;
 }
+function createSideBar() {
+    const sideBar = document.createElement('navbar');
+    sideBar.classList.add('side-bar');
+
+    const ulEl = document.createElement('ul');
+    ulEl.classList.add('ul-bar')
+
+    const listHome = document.createElement('li')
+    listHome.classList.add('list-home');
+    listHome.textContent = 'Home';
+    listHome.classList.add('active')
+
+    const listNotes = document.createElement('li')
+    listNotes.classList.add('list-notes');
+    listNotes.textContent = 'Notes';
+
+    ulEl.appendChild(listHome);
+    ulEl.appendChild(listNotes);
+
+    sideBar.appendChild(ulEl);
+    return sideBar;
+}
 
 function loadPage() {
     const content = document.querySelector('#content');
     const header = createHeader();
     content.appendChild(header);
+
+    const sideBar = createSideBar();
+    content.appendChild(sideBar);
 }
 export default loadPage;
