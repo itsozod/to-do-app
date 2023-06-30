b9822e7e289f43feb550142bfd66a01af2aad744
Ozod <bukhorizodao@gmail.com>
2023-06-30 18:01:37 +0500

    created notes form

--- src/home-form.js
+++ src/home-form.js
@@ -10,9 +10,25 @@ function createHomeForm() {
     `;
     return formContainer;
 };
+function createNotesForm() {
+    const formContainer1 = document.createElement('div');
+    formContainer1.classList.add('form-container1');
+    formContainer1.innerHTML = `
+    <form action="" id="form">
+    <input type="text" id="name" name="name" placeholder="Your notes's name">
+    <button type="submit" id="submit-btn">Submit</button>
+    <div id="nameError" class="error"></div>
+</form>
+    `;
+    return formContainer1;
+};
+
 function loadHomeForm() {
     const main = document.querySelector('#main');
     const formContainer = createHomeForm();
     main.appendChild(formContainer);
+
+    const formContainer1 = createNotesForm();
+    main.appendChild(formContainer1);
 };
 export default loadHomeForm;
--- src/index.js
+++ src/index.js
@@ -1,60 +1,68 @@
 import loadPage from "./initial-load-page";
 import loadHomeForm from "./home-form";
 
-
 function displayHomeOrNotes() {
-    const listHome = document.querySelector('.list-home');
-    const listNotes = document.querySelector('.list-notes');
-    const addHomeContainer = document.querySelector('.add-homeContainer');
-    const addNotesContainer = document.querySelector('.add-notesContainer');
-    const addHomeBtn = document.querySelector('.add-home-btn');
-    const addNotesBtn = document.querySelector('.add-notes-btn');
+  const listHome = document.querySelector(".list-home");
+  const listNotes = document.querySelector(".list-notes");
+  const addHomeContainer = document.querySelector(".add-homeContainer");
+  const addNotesContainer = document.querySelector(".add-notesContainer");
+  const addHomeBtn = document.querySelector(".add-home-btn");
+  const addNotesBtn = document.querySelector(".add-notes-btn");
 
-    listHome.addEventListener('click', () => {
-      // const formContainer = document.querySelector('.form-container');
+  listHome.addEventListener("click", () => {
+    const formContainer1 = document.querySelector(".form-container1");
 
-        listHome.classList.add('active');
-        listNotes.classList.remove('active');
-        addHomeContainer.style.display = 'flex';
-        addNotesContainer.style.display = 'none';
-        // formContainer.style.display = 'none';
-    });
+    listHome.classList.add("active");
+    listNotes.classList.remove("active");
+    addHomeContainer.style.display = "flex";
+    addNotesContainer.style.display = "none";
+    formContainer1.style.display = "none";
+    if (addNotesBtn.classList.contains("active")) {
+      addNotesBtn.classList.remove("active");
+    }
+  });
 
-    listNotes.addEventListener('click', () => {
-      const formContainer = document.querySelector('.form-container');
+  listNotes.addEventListener("click", () => {
+    const formContainer = document.querySelector(".form-container");
 
-        listNotes.classList.add('active');
-        listHome.classList.remove('active');
-        addHomeContainer.style.display = 'none';
-        addNotesContainer.style.display = 'flex';
-        formContainer.style.display = 'none';
-    });
+    listNotes.classList.add("active");
+    listHome.classList.remove("active");
+    addHomeContainer.style.display = "none";
+    addNotesContainer.style.display = "flex";
+    formContainer.style.display = "none";
 
-    addHomeBtn.addEventListener('click', () => {
-      const formContainer = document.querySelector('.form-container');
+    if (addHomeBtn.classList.contains("active")) {
+      addHomeBtn.classList.remove("active");
+    }
+  });
 
-        if (addHomeBtn.classList.contains('active')) {
-          addHomeBtn.classList.remove('active');
-          formContainer.style.display = 'none';
-        } else {
-          addHomeBtn.classList.add('active');
-          formContainer.style.display = 'flex';
-        }
-    });
+  addHomeBtn.addEventListener("click", () => {
+    const formContainer = document.querySelector(".form-container");
 
-    addNotesBtn.addEventListener('click', () => {
-        if (addNotesBtn.classList.contains('active')) {
-          addNotesBtn.classList.remove('active');
-        } else {
-          addNotesBtn.classList.add('active');
-        }
-    });
-};
+    if (addHomeBtn.classList.contains("active")) {
+      addHomeBtn.classList.remove("active");
+      formContainer.style.display = "none";
+    } else {
+      addHomeBtn.classList.add("active");
+      formContainer.style.display = "flex";
+    }
+  });
 
+  addNotesBtn.addEventListener("click", () => {
+    const formContainer1 = document.querySelector(".form-container1");
+    if (addNotesBtn.classList.contains("active")) {
+      addNotesBtn.classList.remove("active");
+      formContainer1.style.display = "none";
+    } else {
+      addNotesBtn.classList.add("active");
+      formContainer1.style.display = "flex";
+    }
+  });
+}
 
 function showPage() {
-    loadPage();
-    displayHomeOrNotes();
-    loadHomeForm();
-};
+  loadPage();
+  displayHomeOrNotes();
+  loadHomeForm();
+}
 showPage();
