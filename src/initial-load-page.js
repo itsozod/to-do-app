fb22dde8f26b70db331c3f05c8e0e85bbb94550b
Ozod <bukhorizodao@gmail.com>
2023-07-04 21:16:00 +0500

    big changes

--- src/initial-load-page.js
+++ src/initial-load-page.js
@@ -50,9 +50,13 @@ function createPage() {
     const main = document.createElement('main');
     main.setAttribute('id', 'main');
 
+    const mainAddHome = document.createElement('div');
+    mainAddHome.classList.add('main-add-home');
+
     // home container
     const addHomeContainer = document.createElement('div');
     addHomeContainer.classList.add('add-homeContainer');
+    mainAddHome.appendChild(addHomeContainer);
     
     const h1Home = document.createElement('h1');
     h1Home.classList.add('h1-home');
@@ -66,19 +70,31 @@ function createPage() {
     addHomeContainer.appendChild(h1Home);
     addHomeContainer.appendChild(addHomeBtn);
     
+    const mainHomeForm = document.createElement('div');
+    mainHomeForm.classList.add('main-home-form');
+
     const homeFormContainer = document.createElement('div');
     homeFormContainer.classList.add('form-container');
+    mainHomeForm.appendChild(homeFormContainer)
+
+    const mainTodos = document.createElement('div');
+    mainTodos.classList.add('main-todos');
 
     const todosContainer = document.createElement('div');
     todosContainer.classList.add('todos-container');
+    mainTodos.appendChild(todosContainer);
 
-    main.appendChild(addHomeContainer);
-    main.appendChild(homeFormContainer);
-    main.appendChild(todosContainer);
+    main.appendChild(mainAddHome);
+    main.appendChild(mainHomeForm);
+    main.appendChild(mainTodos);
 
     // notes container
+    const mainAddNotes = document.createElement('div');
+    mainAddNotes.classList.add('main-add-notes');
+
     const addNotesContainer = document.createElement('div');
     addNotesContainer.classList.add('add-notesContainer');
+    mainAddNotes.appendChild(addNotesContainer);
 
     const h1Notes = document.createElement('h1');
     h1Notes.classList.add('h1-notes');
@@ -92,16 +108,24 @@ function createPage() {
     addNotesContainer.appendChild(h1Notes);
     addNotesContainer.appendChild(addNotesBtn);
 
-    main.appendChild(addNotesContainer);
+    main.appendChild(mainAddNotes);
+
+    const mainNotesForm = document.createElement('div');
+    mainNotesForm.classList.add('main-notes-form');
 
     const notesFormContainer = document.createElement('div');
     notesFormContainer.classList.add('form-container1');
+    mainNotesForm.appendChild(notesFormContainer);
+
+    const mainNotesContainer = document.createElement('div');
+    mainNotesContainer.classList.add('main-notes-container');
 
     const notesContainer = document.createElement('div');
     notesContainer.classList.add('notes-container');
+    mainNotesContainer.appendChild(notesContainer);
 
-    main.appendChild(notesFormContainer);
-    main.appendChild(notesContainer);
+    main.appendChild(mainNotesForm);
+    main.appendChild(mainNotesContainer);
 
     pageContainer.appendChild(main);
     return pageContainer;
--- src/render-notes.js
+++ src/render-notes.js
@@ -9,7 +9,7 @@ function renderNotes() {
     const notesContainer = document.querySelector('.notes-container');
     notesContainer.innerHTML = '';
   
-    myNotes.forEach(note => {
+    myNotes.forEach(function (note) {
       let noteEl = document.createElement('div');
       noteEl.classList.add('notes');
       noteEl.innerHTML = `
@@ -18,7 +18,7 @@ function renderNotes() {
             <img src="./images/delete-button.png" alt="delete" class="delete-note">
             <img src="./images/edit-button.png" alt="delete" class="edit-note">
           </div>
-          <textarea class="description">${note.description}</textarea>
+          <h2 class="description">${note.description}</h2>
           <textarea class="note-name">${note.noteName}</textarea>
         </div>
       `;
@@ -35,8 +35,36 @@ function renderNotes() {
         }, 500);
         console.log('Your note has been deleted');
       });
+
+      // const description = noteEl.querySelector('.description');
+      // const noteName = noteEl.querySelector('.note-name');
+      // const editBtn = noteEl.querySelector('.edit-note');
+
+      // editBtn.addEventListener('click', () => {
+      //   editNotesText(note, description, noteName, editBtn, `#add-back-btn-${index}`);
+      // });
     });
   }
+
+  // function editNotesText(note, description, noteName, editBtn, addBackBtnId) {
+  //   const deskContainer = document.querySelector('.desc-container');
+  //   const addBackBtn = document.createElement('img');
+  //   addBackBtn.src = './images/add-btn.png';
+  //   addBackBtn.id = addBackBtnId;
+  //   addBackBtn.classList.add('add-back-btn');
+
+  //   editBtn.style.display = 'none';
+  //   deskContainer.appendChild(addBackBtn);
+
+  //   const textArea1 = document.createElement('textarea');
+  //   textArea1.value = note.description;
+
+  //   description.replaceWith(textArea1);
+
+  //   const textarea2 = document.createElement('textarea');
+  //   textarea2.value = note.noteName;
+  //   noteName.replaceWith(textarea2);
+  // }
   
 
 function addNotes() {
