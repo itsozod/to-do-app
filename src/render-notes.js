4a74a26cb983a6d3a133d01895dee674559596d3
Ozod <bukhorizodao@gmail.com>
2023-07-05 01:00:29 +0500

    finished with notes

--- src/render-notes.js
+++ src/render-notes.js
@@ -16,9 +16,8 @@ function renderNotes() {
         <div class="note-content">
           <div class="desc-container">
             <img src="./images/delete-button.png" alt="delete" class="delete-note">
-            <img src="./images/edit-button.png" alt="delete" class="edit-note">
           </div>
-          <h2 class="description">${note.description}</h2>
+          <textarea class="description">${note.description}</textarea>
           <textarea class="note-name">${note.noteName}</textarea>
         </div>
       `;
@@ -35,36 +34,9 @@ function renderNotes() {
         }, 500);
         console.log('Your note has been deleted');
       });
-
-      // const description = noteEl.querySelector('.description');
-      // const noteName = noteEl.querySelector('.note-name');
-      // const editBtn = noteEl.querySelector('.edit-note');
-
-      // editBtn.addEventListener('click', () => {
-      //   editNotesText(note, description, noteName, editBtn, `#add-back-btn-${index}`);
-      // });
     });
   }
 
-  // function editNotesText(note, description, noteName, editBtn, addBackBtnId) {
-  //   const deskContainer = document.querySelector('.desc-container');
-  //   const addBackBtn = document.createElement('img');
-  //   addBackBtn.src = './images/add-btn.png';
-  //   addBackBtn.id = addBackBtnId;
-  //   addBackBtn.classList.add('add-back-btn');
-
-  //   editBtn.style.display = 'none';
-  //   deskContainer.appendChild(addBackBtn);
-
-  //   const textArea1 = document.createElement('textarea');
-  //   textArea1.value = note.description;
-
-  //   description.replaceWith(textArea1);
-
-  //   const textarea2 = document.createElement('textarea');
-  //   textarea2.value = note.noteName;
-  //   noteName.replaceWith(textarea2);
-  // }
   
 
 function addNotes() {
